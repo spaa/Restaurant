e6b9f2a4932e789e51b4ccb9258dc4d78420a522
spaa <22143478+spaa@users.noreply.github.com>
2022-01-29 22:10:00 +0530

    Initial commit

---
+++ restro/contactus.php
@@ -0,0 +1,48 @@
+<?php require_once('connection/config.php'); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title><?php echo APP_NAME; ?>:Contacts</title>
+<script type="text/javascript" src="swf/swfobject.js"></script>
+<link href="stylesheets/user_styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="page">
+  <div id="menu"><ul>
+  <li><a href="member-index.php">Home</a></li>
+  <li><a href="foodzone.php">Food Zone</a></li>
+  <li><a href="specialdeals.php">Special Deals</a></li>
+  <li><a href="member-index.php">My Account</a></li>
+  <li><a href="contactus.php">Contact Us</a></li>
+  </ul>
+  </div>
+<div id="header">
+  <div id="logo"> <a href="index.php" class="blockLink"></a></div>
+  <div id="company_name"><!--<?php echo APP_NAME; ?>-->Dwarka Restaurant</div>
+</div>
+<div id="center">
+
+  <h1>Contact Us</h1>
+
+  <div style="border:#bd6f2f solid 1px;padding:4px 6px 2px 6px">
+  <table width="500" height="50">
+  <tr><td rowspan="11"><img width="400" height="400" src="images/map.jpg" /></td></tr>
+  <tr><td rowspan="11"></td></tr>
+  <tr><td><!--<?php echo APP_NAME ?>-->Dwarka Restaurant</td></tr>
+  <tr><td>P.O. Box: 75000-00000</a></td></tr>
+  <tr><td>Website:Online Food Ordering System!</td></tr>
+  <tr><td>ADDRESS:Dwarka Restaurant, Nerul Station, Sector-20, Nerul(west), Navi Mumbai-400706</td></tr>
+  <tr><td>Landline:+012345678</a></td></tr>
+  <tr><td>Mobile:   +9779876543212/+974561230145/+987123456000</a></td></tr>
+  <tr><td>Email:  email.swapnil.chopade@gmail.com </a></td></tr>
+  
+
+  </table>
+  </div>
+</div>
+<?php include 'footer.php'; ?>
+</div>
+
+</body>
+</html>
---
+++ restro/footer.php
@@ -0,0 +1,5 @@
+<div id="footer">
+    <div class="bottom_menu"><a href="index.php">Home Page</a>  |  <a href="aboutus.php">About Us</a>  |  <a href="specialdeals.php">Special Deals</a>  |  <a href="foodzone.php">Food Zone</a>  |  <a href="https://code-projects.org/">Affiliate Program</a> |<br></div>
+
+  <div class="bottom_addr">&copy; 2016 - <?php echo date('Y'); ?> <?php echo APP_NAME; ?>. Developed By Swapnil Chopade</div>
+</div>
